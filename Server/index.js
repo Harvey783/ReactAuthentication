deff145fe2e3b819442d98f6a36cec7b147a0dbf
Christopher Harvey <chharvey@icloud>
2019-06-06 23:03:44 -0400

    initial server setup

---
+++ Server/index.js
@@ -0,0 +1,27 @@
+// node index.js server start
+// npm run dev for nodemon
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost/auth', { useNewUrlParser: true });
+
+// App Setup
+// Logging framework used for debugging
+app.use(morgan('combined'));
+// Any incomming request parsed as JSON
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server Setup
+const port = process.env.PORT || 3001;
+// Creates a http server and forward to express app
+const server = http.createServer(app);
+// Once server created listen to the port
+server.listen(port);
+console.log('Server listening on port', port);
